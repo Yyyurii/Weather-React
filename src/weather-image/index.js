173707384afe61af08815423deff1aa9aa86504d
Yurii <yurii.kryvko@gmail.com>
2020-07-13 23:19:42 +0300

    exportDefault svg

---
+++ src/weather-image/index.js
@@ -0,0 +1,25 @@
+export { default as Icon01d } from './wi-day-sunny.svg';
+export { default as Icon02d } from './wi-day-cloudy.svg';
+export { default as Icon03d } from './wi-cloudy.svg';
+export { default as Icon04d } from './wi-cloudy.svg';
+export { default as Icon09d } from './wi-showers.svg';
+// const dict = {
+//   '01d': 'wi-day-sunny',
+//   '02d': 'wi-day-cloudy',
+//   '03d': 'wi-cloudy',
+//   '04d': 'wi-cloudy',
+//   '09d': 'wi-showers',
+//   '10d': 'wi-day-rain-mix',
+//   '11d': 'wi-thunderstorm',
+//   '13d': 'wi-snow',
+//   '50d': 'wi-fog',
+//   '01n': 'wi-night-clear',
+//   '02n': 'wi-night-alt-cloudy',
+//   '03n': 'wi-night-alt-cloudy-high',
+//   '04n': 'wi-cloudy',
+//   '09n': 'wi-night-alt-sprinkle',
+//   '10n': 'wi-night-alt-showers',
+//   '11n': 'wi-night-alt-thunderstorm',
+//   '13n': 'wi-night-alt-snow',
+//   '50n': 'wi-night-fog',
+// };
